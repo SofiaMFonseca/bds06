29cd43c9fba8d8e3d26fa67448b8b40bc9dae524
Sofia Fonseca <sofia.fonseca277@gmail.com>
2023-09-12 17:32:03 +0100

    Endpoint Movie by id

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.movieflix.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.movieflix.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+	Optional<Movie> findById(Long id);
 
 }
