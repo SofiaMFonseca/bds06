aa594e34b9ba917fccfb5a1c6f0d0e3a040d3980
Sofia Fonseca <sofia.fonseca277@gmail.com>
2023-09-12 19:37:56 +0100

    Endpoint movie reviews

--- src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.MovieService;
+import com.devsuperior.movieflix.services.ReviewService;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -17,10 +21,19 @@ public class MovieResource {
 	@Autowired
 	private MovieService service;
 	
+	@Autowired
+	private ReviewService reviewService;
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
 		MovieDetailsDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@GetMapping(value = "/{movieId}/reviews")
+	public ResponseEntity<List<ReviewDTO>> findMovieReviews(@PathVariable Long movieId) {
+		List<ReviewDTO> list = reviewService.findByMovie(movieId);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
